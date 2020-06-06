18e962c3b740c8e10815642a446ec498fca88de6
jeasilva <jeas560@gmail.com>
2020-06-06 19:56:03 -0300

    added a container in rankNumbers style

--- src/styles/RankNumbers.js
+++ src/styles/RankNumbers.js
@@ -3,46 +3,57 @@ import { StyleSheet, Dimensions } from 'react-native';
 const cardWidth = Dimensions.get('window').width * 0.17;
 const cardHeight = Dimensions.get('window').height * 0.28;
 
+const rankUpDownConstant = 20;
+const rankLeftRightConstant = 10;
+
+const letterWidth = cardWidth / 6;
+const letterheight = cardHeight / 6;
+
 const RankNumberStyles = StyleSheet.create({
   container: {
     position: 'relative',
-    width: cardWidth / 3,
-    height: cardHeight / 4,
+    top: 62.5,
+    left: cardWidth / 3.5,
   },
   rankUp: {
     position: 'absolute',
-    width: cardWidth / 6,
-    height: cardHeight / 6,
-    top: 8,
-    left: 20,
+    width: letterWidth,
+    height: letterheight,
+    resizeMode: 'contain',
+    top: 0,
+    left: rankUpDownConstant,
   },
   rankLeft: {
     position: 'absolute',
-    width: cardWidth / 6,
-    height: cardHeight / 6,
-    top: 25,
-    left: 8,
+    width: letterWidth,
+    height: letterheight,
+    resizeMode: 'contain',
+    top: rankLeftRightConstant,
+    left: 0,
   },
   rankDown: {
     position: 'absolute',
-    width: cardWidth / 6,
-    height: cardHeight / 6,
-    top: 35,
-    left: 20,
+    width: letterWidth,
+    height: letterheight,
+    resizeMode: 'contain',
+    top: 20,
+    left: rankUpDownConstant,
   },
   rankRight: {
     position: 'absolute',
-    width: cardWidth / 6,
-    height: cardHeight / 6,
-    top: 25,
-    left: 35,
+    width: letterWidth,
+    height: letterheight,
+    resizeMode: 'contain',
+    top: rankLeftRightConstant,
+    left: 38,
   },
   element: {
     position: 'absolute',
     width: cardWidth / 3,
     height: cardHeight / 3,
-    top: 10,
-    right: 10,
+    resizeMode: 'contain',
+    top: -52.5,
+    right: cardWidth / 3,
   },
 });
 
